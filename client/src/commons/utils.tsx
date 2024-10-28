b2f9f20c46146352b9102a53ff51483aa8da5d9f
Kammu Sudarshan <skammu@albany.edu>
2024-10-28 16:31:02 -0400

    link update

--- client/src/commons/utils.tsx
+++ client/src/commons/utils.tsx
@@ -1,3 +1,5 @@
 
 
-export const PUBLIC_URL = "http://localhost:9000";
+//export const PUBLIC_URL = "http://localhost:9000";
+
+export const PUBLIC_URL = "http://maintenance-app-server.vercel.app:9000";
