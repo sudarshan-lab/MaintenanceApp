f11bb8ad2ca2449b04874fed7df3cc2a9fd0784f
Kammu Sudarshan <skammu@albany.edu>
2024-10-28 16:55:07 -0400

    checking

--- server/Connection.js
+++ server/Connection.js
@@ -7,7 +7,7 @@ const conParams = {
 }
 
 //const uri = 'mongodb://localhost:27017/'
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.mbt28mb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://skammu:A1s2d3123@cluster0.mbt28mb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connection = mongoose.connect(uri, conParams).then(()=> console.log('connected')).catch((err)=> console.log(err));
 
